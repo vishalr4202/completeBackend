2e771ea8fbb952cc91eb5916efa55ac901bb9a6e
vishalr4202 <vishalr4202@gmsil.com>
2023-12-18 22:58:08 +0530

    cors trial

--- app.js
+++ app.js
@@ -5,36 +5,44 @@ const port = 8001;
 const cluster = require('node:cluster');
 const totalCPUs = require('node:os').cpus().length;
 const process = require('node:process');
-// var cors= require('cors')
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < 2; i++) {
-    cluster.fork();
-  }
+var corsOptions = {
+  origin: 'https://zerodhalogin.netlify.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-  cluster.on('online',function(worker){
-    console.log('Worker is running on %s pid', worker.process.pid);
-  })
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
-  });
+var cors= require('cors')
+// if (cluster.isMaster) {
+//   console.log(`Number of CPUs is ${totalCPUs}`);
+//   console.log(`Master ${process.pid} is running`);
 
-} else {
+//   // Fork workers.
+//   for (let i = 0; i < 2; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('online',function(worker){
+//     console.log('Worker is running on %s pid', worker.process.pid);
+//   })
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     console.log("Let's fork another worker!");
+//     cluster.fork();
+//   });
+
+// }
+//  else {
   const express = require("express");
   const app = express()
   app.use(express.json());
-  // app.use(cors())
+  app.use(cors(corsOptions))
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "OPTIONS, GET, POST, PUT, PATCH, DELETE"
     );
+    res.setHeader("ngrok-skip-browser-warning",true);
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
   });
@@ -58,7 +66,7 @@ if (cluster.isMaster) {
   MongoConnect(() => {
     app.listen(port, () => console.log(`Example app listening on port ${port}!, version 2`));
   });
-}
+// }
 
 
 
